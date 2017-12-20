d2acea9a337415272fe4f609a7beac96ef5bf6e7
fd11 <fd11@FD-XI-14>
2017-12-20 22:23:25 +0700

    add top btn ke-3

--- js/main.js
+++ js/main.js
@@ -129,3 +129,35 @@ counterWayPoint();
 
 
 }());
+
+//---------------------------------------
+//--- TAMBAHAN UNTUK TOMBOL GO TO TOP ---
+//---------------------------------------
+function smsg(a){
+	alert(a);
+}
+
+//--- TAMBAHAN UNTUK BUTTON  GO-TO-TOP ----
+//========Ini Fungsi Untuk Show Button go-to-top========
+
+	window.onscroll = function() {scrollFunction()};
+	
+	function scrollFunction() {
+		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		    document.getElementById("myBtn").style.display = "block";
+		} else {
+		    document.getElementById("myBtn").style.display = "none";
+		}
+	}
+
+	// When the user clicks on the button, scroll to the top of the document
+	function topFunction() {
+		document.body.scrollTop = 0;
+		document.documentElement.scrollTop = 0;
+	}
+//======End Fungsi go-to-top===========
+
+
+
+
+
